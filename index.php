53d7f5e5635cc0b27accb90180a351ce7354af24
alirezajavadi <alirezajavadigit@gmail.com>
2023-12-10 23:58:02 +0330

    add change status routes

--- index.php
+++ index.php
@@ -158,7 +158,11 @@ function dd($var){
 /   :)
 */
 
-// category
+/*
+/   (:
+/   category
+/   :)
+*/
 uri("admin/category", 'Admin\Category', "index");
 uri("admin/category/create", 'Admin\Category', "create");
 uri("admin/category/store", 'Admin\Category', "store", "POST");
@@ -166,13 +170,24 @@ uri("admin/category/edit/{id}", 'Admin\Category', "edit");
 uri("admin/category/update/{id}", 'Admin\Category', "update", "POST");
 uri("admin/category/delete/{id}", 'Admin\Category', "delete");
 
-// post
+/*
+/   (:
+/   post
+/   :)
+*/
 uri("admin/post", 'Admin\Post', "index");
 uri("admin/post/create", 'Admin\Post', "create");
 uri("admin/post/store", 'Admin\Post', "store", "POST");
 uri("admin/post/edit/{id}", 'Admin\Post', "edit");
 uri("admin/post/update/{id}", 'Admin\Post', "update", "POST");
 uri("admin/post/delete/{id}", 'Admin\Post', "delete");
+/*
+/   (:
+/   post change status
+/   :)
+*/
+uri("admin/post/breakingNews/{id}", 'Admin\Post', "breakingNewsChangeStatus");
+uri("admin/post/selected/{id}", 'Admin\Post', "selectedChangeStatus");
 
 
 
