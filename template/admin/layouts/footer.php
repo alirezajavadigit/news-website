1f6fbbe7920070383c34fc476313c0ef7ac6331f
alirezajavadi <alirezajavadigit@gmail.com>
2023-12-10 16:46:16 +0330

    footer and header of my controll panel help me to manage my template easier

---
+++ template/admin/layouts/footer.php
@@ -0,0 +1,24 @@
+
+</main>
+</div>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="<?= asset('public/admin-panel/js/bootstrap.min.js') ?>"></script>
+<script src="<?= asset('public/admin-panel/js/mdb.min.js') ?>"></script>
+<script src="<?= asset('public/ckeditor/ckeditor.js') ?>"></script>
+<script src="<?= asset('public/admin-panel/jalalidatepicker/persian-date.min.js') ?>"></script>
+<script src="<?= asset('public/admin-panel/jalalidatepicker/persian-datepicker.min.js') ?>"></script>
+
+<script>
+$(document).ready(function(){
+        CKEDITOR.replace('summary');
+        CKEDITOR.replace('body');
+})
+
+</script>
+
+</body>
+
+</html>
