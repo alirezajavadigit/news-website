a338ea4c960ab4a0234a82fc639eebea78f1e344
alirezajavadi <alirezajavadigit@gmail.com>
2023-12-11 00:07:29 +0330

    add 2 url to sidebar

--- template/admin/layouts/header.php
+++ template/admin/layouts/header.php
@@ -41,9 +41,9 @@
     <div class="container-fluid">
         <div class="row">
             <nav class="col-md-2 d-none d-md-block pt-3 bg-sidebar sidebar px-0">
-                <a class="text-decoration-none d-block py-1 px-2 mt-1" href=""><i class="fas fa-home"></i> Home</a>
+                <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin') ?>"><i class="fas fa-home"></i> Home</a>
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/category') ?>"><i class="fas fa-clipboard-list"></i> Category</a>
-                <a class="text-decoration-none d-block py-1 px-2 mt-1" href=""><i class="fas fa-newspaper"></i> Post</a>
+                <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/post') ?>"><i class="fas fa-newspaper"></i> Post</a>
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href=""><i class="fas fa-image"></i> Banner</a>
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href=""><i class="fas fa-comments"></i> Comment</a>
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href=""><i class="fas fa-bars"></i> Menus</a>
