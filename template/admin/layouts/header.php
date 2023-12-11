b67ec1d38ad0be8498577417e335d9288b440967
alirezajavadi <alirezajavadigit@gmail.com>
2023-12-11 22:57:01 +0330

    edited header of admin template and completed all sidebar urls

--- template/admin/layouts/header.php
+++ template/admin/layouts/header.php
@@ -45,10 +45,10 @@
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/category') ?>"><i class="fas fa-clipboard-list"></i> Category</a>
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/post') ?>"><i class="fas fa-newspaper"></i> Post</a>
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/banner') ?>"><i class="fas fa-image"></i> Banner</a>
-                <a class="text-decoration-none d-block py-1 px-2 mt-1" href=""><i class="fas fa-comments"></i> Comment</a>
-                <a class="text-decoration-none d-block py-1 px-2 mt-1" href=""><i class="fas fa-bars"></i> Menus</a>
-                <a class="text-decoration-none d-block py-1 px-2 mt-1" href=""><i class="fas fa-users"></i> User</a>
-                <a class="text-decoration-none d-block py-1 px-2 mt-1" href=""><i class=" fas fa-tools"></i> Web Setting</a>
+                <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/comment') ?>"><i class="fas fa-comments"></i> Comment</a>
+                <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/menus') ?>"><i class="fas fa-bars"></i> Menus</a>
+                <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/user') ?>"><i class="fas fa-users"></i> User</a>
+                <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/setting') ?>"><i class=" fas fa-tools"></i> Web Setting</a>
             </nav>
 
             <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
