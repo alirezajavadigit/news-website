2a92dc96bd1483aa6841deb6b39df4188654c9df
alirezajavadi <alirezajavadigit@gmail.com>
2023-12-11 22:53:59 +0330

    User.php completed

---
+++ activities/Admin/User.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Admin;
+
+use database\DataBase;
+
+class User extends Admin
+{
+    public function index()
+    {
+        $db = new DataBase();
+        $users = $db->select("SELECT * FROM users ORDER BY `id` DESC")->fetchAll();
+        view("template.admin.users.index.php", compact('users'));
+    }
+
+    public function changePermission($id)
+    {
+        $db = new DataBase;
+        $users = $db->select("SELECT * FROM users WHERE id = ?", [$id])->fetch();
+        $status = null;
+        if ((int) $users['permission'] == "admin") {
+            $values = array();
+            $values['permission'] = "user";
+            $db->update("users", $id, array_keys($values), $values);
+        } else {
+            $values = array();
+            $values['permission'] = "admin";
+            $db->update("users", $id, array_keys($values), $values);
+        }
+        return $this->redirectBack();
+    }
+
+    public function edit($id)
+    {
+
+        $db = new DataBase();
+        $user = $db->select("SELECT * FROM users WHERE id = ?", [$id])->fetch();
+        view("template.admin.users.edit.php", compact('user'));
+    }
+
+    public function update($request, $id)
+    {
+        $db = new DataBase();
+        $result = $db->update("users", $id, array_keys($request), $request);
+        return $this->redirect("admin/user");
+    }
+
+    public function delete($id)
+    {
+        $db = new DataBase();
+        $result = $db->delete("users", $id);
+        return $this->redirectBack();
+    }
+}
