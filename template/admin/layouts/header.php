a99437335f783215fefc246d21a249311d5934b1
alirezajavadi <alirezajavadigit@gmail.com>
2023-12-12 14:10:05 +0330

    fixed a syntax error

--- template/admin/layouts/header.php
+++ template/admin/layouts/header.php
@@ -46,7 +46,7 @@
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/post') ?>"><i class="fas fa-newspaper"></i> Post</a>
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/banner') ?>"><i class="fas fa-image"></i> Banner</a>
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/comment') ?>"><i class="fas fa-comments"></i> Comment</a>
-                <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/menus') ?>"><i class="fas fa-bars"></i> Menus</a>
+                <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/menu') ?>"><i class="fas fa-bars"></i> Menus</a>
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/user') ?>"><i class="fas fa-users"></i> User</a>
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/setting') ?>"><i class=" fas fa-tools"></i> Web Setting</a>
             </nav>
