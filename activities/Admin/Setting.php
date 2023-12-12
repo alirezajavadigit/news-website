5b58f31495682f9e35bf83b430e15667a1a5cb54
alirezajavadi <alirezajavadigit@gmail.com>
2023-12-12 21:27:05 +0330

    changed setting algorithm completlly

--- activities/Admin/Setting.php
+++ activities/Admin/Setting.php
@@ -11,15 +11,47 @@ class Setting extends Admin{
         view("template.admin.setting.index.php", compact('setting'));
     }
 
-    public function show($id){
+    public function show(){
         $db = new DataBase();
-        $setting = $db->select("SELECT * FROM setting WHERE id = ?", [$id])->fetch();
+        $setting = $db->select("SELECT * FROM setting")->fetch();
         view("template.admin.setting.show.php", compact('setting'));
     }
 
-    public function update($request, $id){
+    public function update($request){
         $db = new DataBase();
-        $result = $db->update("setting", $id, array_keys($request), $request);
+        $setting = $db->select("SELECT * FROM setting")->fetch();
+    
+        if($setting == null){
+            if($request['icon']['tmp_name'] != null){
+                $imageUploadResult = $this->saveImage($request['icon'], 'icon');
+                $request['icon'] = $imageUploadResult;
+            }else{
+                unset($request['icon']);
+            }
+            if($request['logo']['tmp_name'] != null){
+                $imageUploadResult = $this->saveImage($request['logo'], 'logo');
+                $request['logo'] = $imageUploadResult;
+            }else{
+                unset($request['logo']);
+            }
+            $db->insert("setting", array_keys($request), $request);
+        }else{
+            if($request['icon']['tmp_name'] != null){
+                $this->removeImage($setting['icon']);
+                $imageUploadResult = $this->saveImage($request['icon'], 'icon');
+                $request['icon'] = $imageUploadResult;
+            }else{
+                unset($request['icon']);
+            }
+            if($request['logo']['tmp_name'] != null){
+                $this->removeImage($setting['logo']);
+                $imageUploadResult = $this->saveImage($request['logo'], 'logo');
+                $request['logo'] = $imageUploadResult;
+            }else{
+                unset($request['logo']);
+            }
+            $result = $db->update("setting", $setting['id'], array_keys($request), $request);
+        }
         return $this->redirect("admin/setting");
     }
 
