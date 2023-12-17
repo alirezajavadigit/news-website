28b3f2805858d4893ce027c4fa5de823dec16b48
alirezajavadi <alirezajavadigit@gmail.com>
2023-12-17 19:42:49 +0330

    checking is current user admin

--- activities/Admin/Admin.php
+++ activities/Admin/Admin.php
@@ -2,54 +2,76 @@
 
 namespace Admin;
 
+use database\DataBase;
 
-class Admin{
+class Admin
+{
     protected $currentDomain;
     protected $basePath;
 
-    function __construct(){
+    function __construct()
+    {
         $this->currentDomain = CURRENT_DOMAIN;
         $this->basePath = BASE_PATH;
+        $db = new DataBase();
+        if (isset($_SESSION['user'])) {
+
+            $user = $db->select("SELECT * FROM users WHERE id = ?", [$_SESSION['user']])->fetch();
+            if ($user != null) {
+                if ($user['permission'] == "admin") {
+                } else {
+                    return $this->redirect("home");
+                }
+            } else {
+                return $this->redirect("home");
+            }
+        } else {
+            return $this->redirect("home");
+        }
     }
 
-    protected function redirect($to){
+    protected function redirect($to)
+    {
         $to = trim($this->currentDomain, "/ ") . "/" . trim($to, "/ ");
-        header("Location: ". $to);
+        header("Location: " . $to);
         exit();
     }
 
-    protected function redirectBack(){
+    protected function redirectBack()
+    {
         header("Location: " . $_SERVER["HTTP_REFERER"]);
         exit();
     }
 
-    protected function saveImage($image, $path, $imageName = null){
-        if($imageName){
+    protected function saveImage($image, $path, $imageName = null)
+    {
+        if ($imageName) {
             $extention = explode("/", $image['type'])[1];
             $imageName = $imageName . "." . $extention;
-        }else{
+        } else {
             $extention = explode("/", $image['type'])[1];
             $imageName = date("Y-m-d-H-i-s") . "." . $extention;
         }
         $imageTemp = $image['tmp_name'];
         $imagePath = "public/" . $path . "/";
-        if(is_uploaded_file($imageTemp)){
-            if(move_uploaded_file($imageTemp, $imagePath . $imageName)){
+        if (is_uploaded_file($imageTemp)) {
+            if (move_uploaded_file($imageTemp, $imagePath . $imageName)) {
                 return $imagePath . $imageName;
-            }else{
+            } else {
                 return false;
             }
-        }else{
+        } else {
             return false;
         }
     }
-    
-    protected function removeImage($imagePath){
+
+    protected function removeImage($imagePath)
+    {
         $imagePath = trim($imagePath, "/ ");
         // dd(file_exists($imagePath));
-        if(file_exists($imagePath)){
+        if (file_exists($imagePath)) {
             unlink($imagePath);
-        }else{
+        } else {
             return false;
         }
     }
