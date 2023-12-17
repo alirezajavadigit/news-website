89443edca391d181dd8868af262306474bcc4620
alirezajavadi <alirezajavadigit@gmail.com>
2023-12-17 19:55:34 +0330

    admin index template

---
+++ template/admin/index.php
@@ -0,0 +1,22 @@
+<?php
+
+        require_once(BASE_PATH . '/template/admin/layouts/header.php');
+
+
+?>
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h5"><i class="fas fa-newspaper"></i> admin</h1>
+    </div>
+    <div class="table-responsive">
+        <table class="table table-striped table-sm">
+            <caption>welcome</caption>
+        </table>
+    </div>
+
+
+    <?php
+
+        require_once(BASE_PATH . '/template/admin/layouts/footer.php');
+
+
+?>
