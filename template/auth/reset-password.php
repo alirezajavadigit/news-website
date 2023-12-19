b8764195e6574d07b0671b37b10490a6c29f22a6
alirezajavadi <alirezajavadigit@gmail.com>
2023-12-19 16:00:46 +0330

    forgot password templates completed

---
+++ template/auth/reset-password.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>تغییر رمز عبور</title>
+<?php require_once(BASE_PATH . '/template/auth/layouts/head-tag.php')?>
+</head>
+
+<body>
+
+<div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100">
+                <div class="login100-pic js-tilt" data-tilt>
+                <img src="<?= asset('public/auth/assets/images/img-01.png') ?>" alt="IMG">
+                </div>
+
+                <form method="POST" action="<?= url('change-password/' . $token) ?>" class="login100-form validate-form">
+                    <span class="login100-form-title">
+                        Reset Password
+                    </span>
+
+                    <?php
+                        $message = flash('forgot_email_erros');
+                        if (!empty($message)) {
+                            ?>
+
+                    <div class="mb-2 alert alert-danger"> <small class="form-text text-danger">
+                        <?= $message ?>
+                    </small> </div>
+
+                    <?php
+                        } ?>
+
+
+                    <div class="wrap-input100 validate-input" data-validate="Password is required">
+                        <input class="input100" type="password" name="password" placeholder="Password">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-lock" aria-hidden="true"></i>
+                        </span>
+                    </div>
+
+                    <div class="container-login100-form-btn">
+                        <button type="submit" class="login100-form-btn">
+                            Send
+                        </button>
+                    </div>
+
+                    <div class="text-center p-t-12">
+                        <span class="txt1">
+                            Forgot
+                        </span>
+                        <a class="txt2" href="#">
+                            Username / Password?
+                        </a>
+                    </div>
+
+                    <div class="text-center p-t-136">
+                        <a class="txt2" href="<?= url("login") ?>">
+                            Login your Account
+                            <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+
+<?php require_once(BASE_PATH . '/template/auth/layouts/scripts.php')?>
+
+  
+
+</body>
+
+</html>
