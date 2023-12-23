bfffc009ca02215c1b4aa3121df2e2c1187ab8a3
alirezajavadi <alirezajavadigit@gmail.com>
2023-12-23 15:22:07 +0330

    completed header

--- template/app/layouts/header.php
+++ template/app/layouts/header.php
@@ -15,7 +15,15 @@
     <!-- meta character set -->
     <meta charset="UTF-8">
     <!-- Site Title -->
-    <title><?= $setting['title'] ?></title>
+    <?php $title = null;
+        if(isset($setting)){
+            $title = $setting['title'];
+        }else{
+            $title = $post['title'];
+        }
+    
+    ?>
+    <title><?= $title ?></title>
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
     <!--
 		CSS
