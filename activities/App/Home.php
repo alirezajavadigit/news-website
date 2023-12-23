fb10b42169dd81acf527ead9b26b1fbd4a1bd1be
alirezajavadi <alirezajavadigit@gmail.com>
2023-12-23 15:18:36 +0330

    completed 2 method index and postshow

--- activities/App/Home.php
+++ activities/App/Home.php
@@ -32,13 +32,13 @@ class Home
     public function index()
     {
         $db = new DataBase();
-        $selectedPosts = $db->select("SELECT posts.*, (SELECT name FROM categories WHERE posts.cat_id = categories.id) as category_name, (SELECT username FROM users WHERE posts.user_id = users.id) as user_name, (SELECT COUNT(*) FROM comments WHERE posts.id = comments.post_id) as comment_count FROM posts WHERE posts.selected = 1 ORDER BY posts.created_at DESC LIMIT 0,3")->fetchAll();
+        $selectedPosts = $db->select("SELECT posts.*, (SELECT name FROM categories WHERE posts.cat_id = categories.id) as category_name,(SELECT id FROM categories WHERE posts.cat_id = categories.id) as category_id, (SELECT username FROM users WHERE posts.user_id = users.id) as user_name, (SELECT COUNT(*) FROM comments WHERE posts.id = comments.post_id) as comment_count FROM posts WHERE posts.selected = 1 ORDER BY posts.created_at DESC LIMIT 0,3")->fetchAll();
         $setting = $db->select("SELECT * FROM setting")->fetch();
         $menus = $db->select("SELECT * FROM menus WHERE parent_id IS NULL")->fetchAll();
         $breakingNews = $db->select("SELECT * FROM posts WHERE breaking_news = 1 ORDER BY created_at DESC")->fetch();
-        $lastsixPosts = $db->select("SELECT posts.*, (SELECT name FROM categories WHERE posts.cat_id = categories.id) as category_name, (SELECT username FROM users WHERE posts.user_id = users.id) as user_name, (SELECT COUNT(*) FROM comments WHERE posts.id = comments.post_id) as comment_count FROM posts WHERE posts.selected = 1 ORDER BY posts.created_at DESC LIMIT 0,6")->fetchAll();
-        $mostViewedPosts = $db->select("SELECT posts.*, (SELECT name FROM categories WHERE posts.cat_id = categories.id) as category_name, (SELECT username FROM users WHERE posts.user_id = users.id) as user_name, (SELECT COUNT(*) FROM comments WHERE posts.id = comments.post_id) as comment_count FROM posts ORDER BY posts.view DESC LIMIT 0,3")->fetchAll();
-        $mostCommentedPosts = $db->select("SELECT posts.*, (SELECT name FROM categories WHERE posts.cat_id = categories.id) as category_name, (SELECT username FROM users WHERE posts.user_id = users.id) as user_name, (SELECT COUNT(*) FROM comments WHERE posts.id = comments.post_id) as comment_count FROM posts ORDER BY comment_count DESC LIMIT 0,6")->fetchAll();
+        $lastsixPosts = $db->select("SELECT posts.*, (SELECT name FROM categories WHERE posts.cat_id = categories.id) as category_name, (SELECT id FROM categories WHERE posts.cat_id = categories.id) as category_id, (SELECT username FROM users WHERE posts.user_id = users.id) as user_name, (SELECT COUNT(*) FROM comments WHERE posts.id = comments.post_id) as comment_count FROM posts WHERE posts.selected = 1 ORDER BY posts.created_at DESC LIMIT 0,6")->fetchAll();
+        $mostViewedPosts = $db->select("SELECT posts.*, (SELECT name FROM categories WHERE posts.cat_id = categories.id) as category_name, (SELECT id FROM categories WHERE posts.cat_id = categories.id) as category_id, (SELECT username FROM users WHERE posts.user_id = users.id) as user_name, (SELECT COUNT(*) FROM comments WHERE posts.id = comments.post_id) as comment_count FROM posts ORDER BY posts.view DESC LIMIT 0,3")->fetchAll();
+        $mostCommentedPosts = $db->select("SELECT posts.*, (SELECT name FROM categories WHERE posts.cat_id = categories.id) as category_name, (SELECT id FROM categories WHERE posts.cat_id = categories.id) as category_id, (SELECT username FROM users WHERE posts.user_id = users.id) as user_name, (SELECT COUNT(*) FROM comments WHERE posts.id = comments.post_id) as comment_count FROM posts ORDER BY comment_count DESC LIMIT 0,6")->fetchAll();
 
         return view("template.app.index.php", compact("setting", "selectedPosts", "menus", "breakingNews", "lastsixPosts", "mostViewedPosts", "mostCommentedPosts"));
     }
@@ -46,8 +46,26 @@ class Home
     public function postShow($id)
     {
         $db = new DataBase();
-        $post = $db->select("SELECT* FROM posts WHERE id = ?", [$id])->fetch();
-        return view("template.app.show.php", compact("post"));
+        $selectedPosts = $db->select("SELECT posts.*, (SELECT name FROM categories WHERE posts.cat_id = categories.id) as category_name,(SELECT id FROM categories WHERE posts.cat_id = categories.id) as category_id, (SELECT username FROM users WHERE posts.user_id = users.id) as user_name, (SELECT COUNT(*) FROM comments WHERE posts.id = comments.post_id) as comment_count FROM posts WHERE posts.selected = 1 ORDER BY posts.created_at DESC LIMIT 0,3")->fetchAll();
+        $menus = $db->select("SELECT * FROM menus WHERE parent_id IS NULL")->fetchAll();
+        $post = $db->select("SELECT posts.*, (SELECT name FROM categories WHERE posts.cat_id = categories.id) as category_name, (SELECT id FROM categories WHERE posts.cat_id = categories.id) as category_id, (SELECT username FROM users WHERE posts.user_id = users.id) as user_name, (SELECT COUNT(*) FROM comments WHERE posts.id = comments.post_id) as comment_count FROM posts WHERE id = ?", [$id])->fetch();
+        $mostCommentedPosts = $db->select("SELECT posts.*, (SELECT name FROM categories WHERE posts.cat_id = categories.id) as category_name, (SELECT id FROM categories WHERE posts.cat_id = categories.id) as category_id, (SELECT username FROM users WHERE posts.user_id = users.id) as user_name, (SELECT COUNT(*) FROM comments WHERE posts.id = comments.post_id) as comment_count FROM posts ORDER BY comment_count DESC LIMIT 0,6")->fetchAll();
+        $allPosts = $db->select("SELECT * FROM posts")->fetchAll();
+        // foreach($allPosts as $key => $onePost){
+        //     if($onePost['id'] == $id){
+        //         unset($allPosts[$key]);
+        //     }
+        // }
+        $nextPostId = next($allPosts)['id'];
+        $preventPostId = prev($allPosts)['id'];
+        // dd($nextPostId);
+        foreach($selectedPosts as $key => $selectedPost){
+            if($selectedPost['id'] == $id){
+                unset($selectedPosts[$key]);
+            }
+        }
+        $comments = $db->select("SELECT comments.*, (SELECT username FROM users WHERE comments.user_id = users.id) as user_name FROM comments WHERE post_id = ?", [$id])->fetchAll();
+        return view("template.app.show.php", compact("post", "comments", "selectedPosts", "menus", "mostCommentedPosts", "nextPostId", "preventPostId"));
     }
     public function categoryShow($id)
     {
